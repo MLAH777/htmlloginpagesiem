84c0dbb36038a294f87b79c3faf9106be8d0f421
Aashik Harishwar M L <112604441+MLAH777@users.noreply.github.com>
2025-05-16 15:59:12 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ const cors = require('cors');
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Middleware
 app.use(express.json());
